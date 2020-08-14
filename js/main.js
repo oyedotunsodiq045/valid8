c0edbb750c5f4d4e3ddeafb68ad24edecf2b05bd
sodiq oyedotun <oyedotunsodiq045@gmail.com>
2020-08-14 08:53:16 +0100

    final commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-// https://www.textmagic.com/free-tools/phone-validator
-
 function isLetter(str) {
   return str.match(/[a-z]/i);
 }
@@ -10,8 +8,7 @@ function validate() {
   const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
   const num = param.toString().length;
   const result = document.getElementById('result');
-
-  // Ternary
+  
   isLetter(res)
     ? reg.test(input.value) == false
       ? ((result.style.color = 'red'),
@@ -23,25 +20,4 @@ function validate() {
         (result.innerHTML = 'Valid Number <=> ' + param))
       : ((result.style.color = 'red'),
         (result.innerHTML = 'Invalid Number <=> ' + param));
-
-  // If...Else
-  // if (isLetter(res)) {
-  //   // Check for Email
-  //   if (reg.test(input.value) == false) {
-  //     result.style.color = 'red';
-  //     result.innerHTML = 'Invalid email => ' + param;
-  //   } else {
-  //     result.style.color = 'DarkGreen';
-  //     result.innerHTML = 'Valid Email => ' + param;
-  //   }
-  // } else {
-  //   // Check for Number
-  //   if (num > 10) {
-  //     result.style.color = 'DarkGreen';
-  //     result.innerHTML = 'Valid Number => ' + param;
-  //   } else {
-  //     result.style.color = 'red';
-  //     result.innerHTML = 'Invalid Number => ' + param;
-  //   }
-  // }
 }
